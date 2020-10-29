2eac7ec0709104035b5c1630ba08c32c5086f37b
danneg <dana@boxalino.com>
2020-10-29 21:35:44 +0100

    [sorting] sorting option migrated to rtux-api-php

--- Model/Response/Content/ApiSorting.php
+++ Model/Response/Content/ApiSorting.php
@@ -2,8 +2,6 @@
 namespace Boxalino\RealTimeUserExperience\Model\Response\Content;
 
 use Boxalino\RealTimeUserExperienceApi\Framework\Content\Listing\ApiSortingModelInterface;
-use Boxalino\RealTimeUserExperienceApi\Service\Api\Response\Accessor\AccessorInterface;
-use Boxalino\RealTimeUserExperienceApi\Service\Api\Response\Accessor\AccessorModelInterface;
 use Boxalino\RealTimeUserExperienceApi\Framework\Content\Listing\ApiSortingModelAbstract;
 
 /**
@@ -31,25 +29,7 @@ class ApiSorting extends ApiSortingModelAbstract
     public function __construct(array $sortingList)
     {
         parent::__construct();
-        foreach($sortingList as $urlKey => $sortDefinition)
-        {
-            $sortingOption = new ApiSortingOption($sortDefinition);
-            $this->add([$sortingOption->getField() => $sortingOption->getApiField()]);
-            $this->addSystemSortingOption($urlKey, $sortingOption);
-        }
-    }
-
-    /**
-     * Adding system sort options per project integration specifications
-     *
-     * @param string $key
-     * @param ApiSortingOption $option
-     * @return $this
-     */
-    public function addSystemSortingOption(string $key, ApiSortingOption $option) : self
-    {
-        $this->sortings[$key] = $option;
-        return $this;
+        $this->addSortingOptionCollection($sortingList);
     }
 
     /**
@@ -70,91 +50,6 @@ class ApiSorting extends ApiSortingModelAbstract
     public function getDefaultSortDirection() : string
     {
         return ApiSortingModelInterface::SORT_ASCENDING;
-    }
-
-    /**
-     * Transform a request key to a valid API sort
-     *
-     * @param string $key
-     * @return array
-     */
-    public function getRequestSorting(string $key) : array
-    {
-        $requestedSortingList = [];
-        if($this->has($key))
-        {
-            $requestedSortingList[] = [
-                "field" => $this->get($key)->getApiField(),
-                "reverse" => $this->get($key)->isReverse()
-            ];
-        }
-
-        return $requestedSortingList;
-    }
-
-    /**
-     * Based on the response,
-     * transforms the response field and direction into a e-shop valid sorting
-     */
-    public function getCurrent() : string
-    {
-        $responseField = $this->getCurrentApiSortField();
-        if(!empty($responseField))
-        {
-            $direction = $this->getCurrentSortDirection();
-            $field = $this->getResponseField($responseField);
-            foreach($this->getSortings() as $key => $sorting)
-            {
-                /** @var $sorting ApiSortingOption */
-                if($sorting->getField() == $field && $sorting->getDirection() == $direction)
-                {
-                    return $key;
-                }
-            }
-        }
-
-        return $this->getDefaultSortField();
-    }
-
-    /**w
-     * @param string $key
-     * @return ApiSortingOption |null
-     */
-    public function get(string $key) : ?ApiSortingOption
-    {
-        return $this->sortings[$key] ?? null;
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    public function has(string $key): bool
-    {
-        return isset($this->sortings[$key]);
-    }
-
-    /**
-     * Accessing the sort options available for the e-shop
-     *
-     * @return array
-     */
-    public function getSortings(): array
-    {
-        return $this->sortings;
-    }
-
-    /**
-     * The getter "getSorting" is used in accordance to the property name assigned for the bx-sort accessor
-     * (in di.xml)
-     *
-     * @param null | AccessorInterface $context
-     * @return AccessorModelInterface
-     */
-    public function addAccessorContext(?AccessorInterface $context = null): AccessorModelInterface
-    {
-        $this->setActiveSorting($context->getSorting());
-        return $this;
     }
 
 }
